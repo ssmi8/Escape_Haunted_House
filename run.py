91d6d4a9b7900782322a7f8c33451a35757cfa63
ssmi8 <smithsteve8@hotmail.com>
2022-05-29 15:19:44 +0000

    Update: name of title

--- run.py
+++ run.py
@@ -1,17 +1,20 @@
 # Creator: Steven Smith
 
+def intro():
+    print('Introduction to the story line')
+
 print()
-print("    ##########################")
-print("    #                        #")
-print("    #  Escape Haunted House  #")
-print("    #                        #")
-print("    ##########################")
+print("    #########################")
+print("    #                       #")
+print("    #  Escape Horror House  #")
+print("    #                       #")
+print("    #########################")
 print()
 print()
 
-start_game = input("Would y ou like to start the game? (Y/N):\n")
+start_game = input("Would you like to start the game? (Y/N):\n")
 if start_game == 'n' or start_game == 'N':
     print("Okay...maybe next time")
-elif start_game == 'y' or start_game == 'Y'
+elif start_game == 'y' or start_game == 'Y':
     intro()
 
