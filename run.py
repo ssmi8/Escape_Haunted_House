8d6204d9dddb056f883250c76ce20c731c91fb3b
ssmi8 <smithsteve8@hotmail.com>
2022-05-29 13:10:36 +0000

    feat: Welcome message and start game function

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Creator: Steven Smith
+
+print()
+print("    ##########################")
+print("    #                        #")
+print("    #  Escape Haunted House  #")
+print("    #                        #")
+print("    ##########################")
+print()
+print()
+
+start_game = input("Would y ou like to start the game? (Y/N):\n")
+if start_game == 'n' or start_game == 'N':
+    print("Okay...maybe next time")
+elif start_game == 'y' or start_game == 'Y'
+    intro()
+
