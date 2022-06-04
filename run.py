92f3ef2fb2197dc006f241bc2832a67e9815174b
ssmi8 <smithsteve8@hotmail.com>
2022-06-04 09:30:29 +0000

    Update: Intro to story and first option for user

--- run.py
+++ run.py
@@ -1,7 +1,28 @@
 # Creator: Steven Smith
 
 def intro():
-    print('Introduction to the story line')
+    print("(Everything is dark)")
+    print("You look around to try and figure out where you are")
+    print("The last thing you rememeber was walking to your car and then everything went black")
+    print("You can feel old, damp carpet around")
+    print("Your eyes slowly adjust, and you start to make out that your in a strange room")
+    print()
+    print('"Where am I?  Whose room is this?"')
+    print()
+    print('A light shines through a crack in the window and shines onto the door...."Is this my way out?"')
+    print("You walk towards the door...you notice there is no lock")
+    print("You gently push the door open")
+    print("In front you of you are three more doors")
+    print()
+    print("Door #1: Looks old and worn.  There are burn marks on it...")
+    print("Door #2: It's a wooden door. You look closely and can see scratch marks on the door")
+    print("Has someone been here before?")
+    print("Door #3: The last door looks plain, almost new...strange")
+    print()
+    print()
+    firstPath = input("Which door will you choose? (1/2/3)")
+
+
 
 print()
 print("    #########################")
