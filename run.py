0e4f428c214be4eb6057240dfd596acf158d9e13
ssmi8 <smithsteve8@hotmail.com>
2022-06-05 09:41:03 +0000

    Update: Winning route created

--- run.py
+++ run.py
@@ -78,13 +78,68 @@ def path3():
     secondPath = input("Which path will you take? (1/2/3/4/5:\n")
     if secondPath == '2':
         path2_2()
+    elif secondPath == '3':
+        path1_1()
+    elif secondPath == '4':
+        path1()
+    elif secondPath == '5':
+        path2()
+    else:
+        print()
+    print("You hide under the desk")
+    print("Not making a sound")
+    print("Slowly the door to room opens with a creek")
+    print("Thud, Thud, Thud")
+    print("....They're here")
+    print()
+    print("Path #1: Stay hidden until they leave")
+    print("Path #2: Make a run for it!")
+    thirdPath = input("Which path would you like to take? (1/2):\n")
+    if thirdPath == '1':
+        path3_1()
+    elif thirdPath == '2':
+        path3_2()
 
 
 def path3_1():
     print()
+    print("You stay under the desk...")
+    print("...hoping they'll leave")
+    print("There is a sudden pause and then silence....")
+    print("You hear heavy breathing")
+    print('"HAHAHA I found you little one!')
+    print("GAME OVER - Try Again!")
+    intro()
 
 def path3_2():
-    Print()
+    Print() #Winning path
+    print("You realise you have a key, this must be the way out")
+    print("Slowly, you lean out and notice the door is open")
+    print("It's now or never...")
+    print('You whisper "I can do this, I can do this"')
+    print("You wait for your moment, they have they're back to you")
+    print("In one swift motion you get and up and run for the door")
+    print("Down the corridor you see a set of stairs")
+    print("Without hesitation you bolt to the bottom")
+    print("...")
+    print("You look around for freedom")
+    print("There's a door behind you")
+    print("Escape is near")
+    print("But it's locked")
+    print("You can hear footsteps getting louder")
+    print("They're coming for you")
+    print(".....the key!")
+    print("You remember you found a key in the other room")
+    print("It works!")
+    print("They're nearly at the bottom of the stairs")
+    print("You only have seconds left")
+    print("With one big push, the door flings open")
+    print("You run towards the street for help")
+    print("A car is passing by")
+    print("You stop them and ask for help")
+    print("You made it! You're safe now")
+    print()
+    print("Thanks for playing!!")
 
 
 
